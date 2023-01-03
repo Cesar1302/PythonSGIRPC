f5c5bbeb34d8b85a89a5337b2f650c10f368dbe9
Cesar1302 <jclm1302@outlook.es>
2023-01-03 15:06:52 -0600

    actualizado

---
+++ transparentarimagenes.bat
@@ -0,0 +1,4 @@
+convert  "C:\Users\meteorologia\Documents\Mapas\DATOS\SGIRPC.png" -resize 4000x2000 -quality 100 "C:\Users\meteorologia\Documents\Mapas\DATOS\SGIRPC2023.png"
+
+convert "C:\Users\meteorologia\Documents\Mapas\DATOS\SGIRPC2023.png" -fuzz 20% -transparent white "C:\Users\meteorologia\Documents\Mapas\DATOS\SGIRPC2023.png"
+
