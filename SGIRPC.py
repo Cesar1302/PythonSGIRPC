f6f443c4c7fa5c1d0b9a14f4037fbef92f6fbb0f
Cesar1302 <jclm1302@outlook.es>
2023-01-09 12:32:40 -0600

    modulo actualizado

--- SGIRPC.py
+++ SGIRPC.py
@@ -7,18 +7,71 @@ import threading
 
 def timer(timer_runs):
     while timer_runs.is_set():
-        ModuloSGIRPC.proceso("iztacalco","AGOS")
-        ModuloSGIRPC.proceso("azcapotzalco","FERS")
-        ModuloSGIRPC.proceso("cuautepec","CUAUS")
-        ModuloSGIRPC.proceso("cuajimalpa","STFS")
-        ModuloSGIRPC.proceso("juarez", "SGIRPC")
-        ModuloSGIRPC.proceso("miguelhidalgo","LEGS")
-        ModuloSGIRPC.proceso("milpaalta","MPAS")
-        ModuloSGIRPC.proceso("iztapa1","LOMS")
-        ModuloSGIRPC.proceso("topilejo","TPJS")
-        ModuloSGIRPC.proceso("coyoacan","SURS")
-        ModuloSGIRPC.proceso("xochimilco","TLHS")
+        try:
+            ModuloSGIRPC.proceso("iztacalco","AGOS")
+        except:
+            print("Hubo un problema con la descarga de datos de estaestación")
+            continue
+        
+        try:
+            ModuloSGIRPC.proceso("azcapotzalco","FERS")
+        except:
+            print("Hubo un problema con la descarga de datos de estaestación")
+            continue
+        
+        try:
+            ModuloSGIRPC.proceso("cuautepec","CUAUS")
+        except:
+            print("Hubo un problema con la descarga de datos de estaestación")
+            continue
+        
+        try:
+            ModuloSGIRPC.proceso("cuajimalpa","STFS")
+        except:
+            print("Hubo un problema con la descarga de datos de estaestación")
+            continue
 
+        try:
+            ModuloSGIRPC.proceso("juarez", "SGIRPC")
+        except:
+            print("Hubo un problema con la descarga de datos de estaestación")
+            continue
+
+        try:
+            ModuloSGIRPC.proceso("miguelhidalgo","LEGS")
+        except:
+            print("Hubo un problema con la descarga de datos de estaestación")
+            continue
+
+        try:
+            ModuloSGIRPC.proceso("milpaalta","MPAS")
+        except:
+            print("Hubo un problema con la descarga de datos de estaestación")
+            continue
+
+        try:
+            ModuloSGIRPC.proceso("iztapa1","LOMS")
+        except:
+            print("Hubo un problema con la descarga de datos de estaestación")
+            continue
+
+        try:
+            ModuloSGIRPC.proceso("topilejo","TPJS")
+        except:
+            print("Hubo un problema con la descarga de datos de estaestación")
+            continue
+
+        try:
+            ModuloSGIRPC.proceso("coyoacan","SURS")
+        except:
+            print("Hubo un problema con la descarga de datos de estaestación")
+            continue
+
+        try:
+            ModuloSGIRPC.proceso("xochimilco","TLHS")
+        except:
+            print("Hubo un problema con la descarga de datos de estaestación")
+            continue
 
         final=datetime.now()
         print(final)
