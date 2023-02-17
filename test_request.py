39e8c12b539ad937aa431e20a92cd0070151c6c5
Cesar1302 <jclm1302@outlook.es>
2023-02-16 21:04:15 -0600

    metar

--- test_request.py
+++ test_request.py
@@ -1,25 +1,35 @@
 import urllib.request
+import requests
 import pandas as pd
+from bs4 import BeautifulSoup
+import csv
 
 url="https://www.aviationweather.gov/metar/data?ids=mmmx&format=raw&date=&hours=48"
-dirmmmx= r'C:\Users\meteorologia\Downloads\MMMX.txt'
-dirmmmxmetar= r'C:\Users\meteorologia\Downloads\MMMXmetar.txt'
+filein= r'C:\Users\meteorologia\Downloads\MMMX.txt'
+fileout= r'C:\Users\meteorologia\Downloads\MMMXmetar.txt'
 
-try:  
-    r = urllib.request.urlopen(url)
-    f = open(dirmmmx,'wb')
-    f.write(r.read())
-    #print(f)
-    f.close()
+#Peticion para compiar los datos del html del METAR
+ 
+r = requests.get(url)
+soup= BeautifulSoup(r.content, "html.parser")
+# tag=soup.find("code")
+# datos=tag.text
+# print(datos)
 
-    datos = []
-    with open(dirmmmx) as metar:
-        lineas= metar.readlines()
-        for linea in lineas:
-            datos.append(linea.strip("METAR"))
-    print(datos)
-    
-    
-    print('Datos de MMMX obtenidos')
-except:
-    print("Se produjo un error al momento de descargar los datos")
+r = urllib.request.urlopen(url)
+f = open(filein,'wb')
+f.write(r.read())
+f.close()
+#print(file1)
+print('Datos del html del METAR descargados')
+
+with open(filein,"r") as fr:
+    contenido= fr.read()
+
+soup= BeautifulSoup(contenido,"html.parser")
+tags_code=soup.find_all("code")
+with open(fileout,"w") as f:
+    for etiqueta in tags_code:
+        tags_content=etiqueta.text
+        print(tags_content)
+        f.write(tags_content + "\n")
